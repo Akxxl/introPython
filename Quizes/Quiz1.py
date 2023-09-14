11f60f62e9985150db21467cd0ae62b90609f9e7
Akxxl <144712877+Akxxl@users.noreply.github.com>
2023-09-14 17:00:48 +0000

    quiz work

--- Quizes/Quiz1.py
+++ Quizes/Quiz1.py
@@ -1,23 +1,36 @@
 # 1. In your on words, describe what a variable is.
+"a whole number" 
 
 # 2. Name three (3) operators we've learned about in class and describe 
 # a situation where you would use each one. 
+"arismetic operator"
+"assignment operator"
+"logical operator"
 
 # 3. Describe the difference between camal case and pascal case.
-
+for camel case ever begining of a word is capital except the first word and fir pascel every begining of a word is capital
+    
 # 4. Which operator is being used in the example below
-numberOfApples = 10
-numberOfOranges = 12
+numberOfApples = 10   "assignment operators"
+numberOfOranges = 12  "assignment operators"
 
-numberOfApples < numberOfOranges
+numberOfApples < numberOfOranges  "comparison operators"
 
 # 5. You have been given a $100.00 dollar gift card from a family member. Another 
 # family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
 
+giftCardGiven = 100.00
+addedToGiftCard = 50.00
+friendGivesYou = 35.00
+
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
+
+question = "what is your name"
+
+input(question)
 
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
@@ -25,12 +38,26 @@ numberOfApples < numberOfOranges
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
 
+name = input("what is your full name")
+age = input("how old are you")
+alllergy = input("do u have any allergys")
+favFood = input("what is your favorite food")
+
+
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
 # If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
 # have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
 
+"Boolean data type"
+
+allergy = input("do you have a gluten allergy")
+yesNo = input("do you want gluten free bread")
+
 # 9. In your own words, describe what a program is?
 
+"a program is something that controls stuff""
+
 # 10. Describe the difference between a float and an integer. 
+"a fleat is a number with a decimal and an integer is a whole number"
