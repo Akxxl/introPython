76f21bc8f638a2159c3fd059bde3b4c0a0e246fc
Akxxl <144712877+Akxxl@users.noreply.github.com>
2023-09-15 16:53:27 +0000

    starting function

---
+++ introPythonActivities/readingActivity.py
@@ -0,0 +1,13 @@
+# read the following articles on Python functions
+"https://www.freecodecamp.org/news/functions-in-python-a-beginners-guide"
+
+# In your own words describe wut a function is
+
+print
+input
+
+def function(){
+
+    x + 3
+    return x
+}
