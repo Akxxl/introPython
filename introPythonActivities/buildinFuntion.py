b61cd08d78764015811e836dbcb57b577b121a3d
Akxxl <144712877+Akxxl@users.noreply.github.com>
2023-09-20 17:03:14 +0000

    finished

--- introPythonActivities/buildinFuntion.py
+++ introPythonActivities/buildinFuntion.py
@@ -1,3 +1,9 @@
 name = "Akeel Ali"
 
-type(name)
+print(type(name))
+
+number_Of_Cars_In_Parking_Lot = 60
+
+print(type(number_Of_Cars_In_Parking_Lot))
+
+ 
---
+++ introPythonActivities/dataCastingActivity.py
@@ -0,0 +1,27 @@
+# convert the following datatypes into floats.
+number_Of_Chicken_Wings = 12
+number_Of_Fries = 100
+# print(float(number_Of_Chicken_Wings), float(number_Of_Fries))
+
+# convert the following floats into integers.
+
+money_In_My_wallet = 12.34
+money_In_My_Cash_App = 23.33
+
+# print(int(money_In_My_wallet))
+# print(int(money_In_My_Cash_App))
+
+# modify the code below to print an integer number. 
+num_one = 20
+num_two= 23.45
+
+# print(int(num_one + num_two))
+
+# modify the following code below to work correctly
+name_of_pet = "Spuny"
+age_of_pet = str(10)
+dog = "this is my dog."
+he_Is = "He is"
+year_Old = "years old."
+
+print(dog + name_of_pet + he_Is + age_of_pet+ year_Old)
