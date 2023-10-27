bb2d4cfbfdb6432a6c240a560912b2a853ba0f5b
Akxxl <144712877+Akxxl@users.noreply.github.com>
2023-10-27 16:39:03 +0000

    cecium Quuestions

---
+++ introPythonActivities/ceciumQuestions.py
@@ -0,0 +1,13 @@
+#Research and develop three (3) questions for Cesium.
+#Your questions should be relevant and appropriate for a workplace setting.
+#Your questions will be screened prior to leaving for the trip. 
+#Questions must be submitted before the end of class for credit. 
+
+#https://cesium.com/
+
+#https://www.betterup.com/blog/how-to-ask-good-questions
+
+
+'1. what is the work evirment like and what are the people like????'
+'2. What things do u offer in u work space for employes'
+'3. how would i sign up or apply to join your company'
