c4ce9663670ce890002524334ba37a73d08eb498
Akxxl <144712877+Akxxl@users.noreply.github.com>
2023-11-02 16:52:13 +0000

    same thing

---
+++ githubCloneFuntionActivity/README.md
@@ -0,0 +1,1 @@
+Run the code an type in what you would like it to reverse
---
+++ githubCloneFuntionActivity/name_gitnameCloneFunction.py
@@ -0,0 +1,9 @@
+def thing():
+
+    word1 = input('what would u like me to reverse?? ')
+
+    print(word1[::-1])
+
+thing()
+
+
